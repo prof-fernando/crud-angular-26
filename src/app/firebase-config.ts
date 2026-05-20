93dbece361a88595442d63e5e8ec7ea71958b842
prof-fernando <fernando.oliveira@iffarroupilha.edu.br>
2026-05-19 21:04:12 -0300

    Update firebase-config.ts

--- src/app/firebase-config.ts
+++ src/app/firebase-config.ts
@@ -1,8 +1,3 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBCndCh1bObI_zmizC6WiWZV9pQC82-okc",
-  authDomain: "aula-27-04-9c93f.firebaseapp.com",
-  projectId: "aula-27-04-9c93f",
-  storageBucket: "aula-27-04-9c93f.firebasestorage.app",
-  messagingSenderId: "791785877108",
-  appId: "1:791785877108:web:e002179db77cd965426b21"
+ 
 };
