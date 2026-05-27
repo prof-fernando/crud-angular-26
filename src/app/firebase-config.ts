bdb372439f82467cf9135d2d51bf0aed9463762a
Fernando <fernando@Fernandos-MacBook-Pro.local>
2026-05-26 21:08:28 -0300

    add codes

--- src/app/firebase-config.ts
+++ src/app/firebase-config.ts
@@ -1,8 +1,1 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyBpnga8NhLbjr6uZW44auUWkWrvKxqWDto',
-  authDomain: 'aula-27-04-f0255.firebaseapp.com',
-  projectId: 'aula-27-04-f0255',
-  storageBucket: 'aula-27-04-f0255.firebasestorage.app',
-  messagingSenderId: '304548054880',
-  appId: '1:304548054880:web:e437819e9204d6725f4005',
-};
+export const firebaseConfig = {};
